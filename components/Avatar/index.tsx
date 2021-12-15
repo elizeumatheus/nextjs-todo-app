82f6dea4c44fc8f4910626601e5ca7bf940cfe83
Elizeu Louzeiro <emjlouzeiro@gmail.com>
2021-12-14 23:16:09 -0300

    feat: wrapped the avatar with a container

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import styles from "./Avatar.module.scss";
 
 export type AvatarProps = {
   src: string;
   name: string;
 };
 
-export const Avatar = ({ src, name }: AvatarProps) => {
-  return <Image src={src} width={60} height={60} alt={name} />;
-};
+export const Avatar = ({ src, name }: AvatarProps) => (
+  <div className={styles.container}>
+    <Image src={src} width={60} height={60} alt={name} />
+  </div>
+);
